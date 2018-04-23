d07a34287cf33252197ff8e83b1e70543586d8b0
TriciaF <TriciaF@thinkful.com>
2018-04-23 14:43:26 -0400

    made the project images show up in responsive mode

--- src/About.js
+++ src/About.js
@@ -14,9 +14,9 @@ class About extends React.Component {
       <div className='about-desc'>
       <div className='about-desc-paragraph'>My name is Patricia Forrester, but please call me Tricia.  I am an experienced software developer with technical skills ranging from embedded C++ to full-stack web development.  In addition to software development, as a product manager, I defined customer requirements and managed product development including, medical devices, military communication systems, and commercial security systems.  I consider myself experienced in software development best practices and achieved my Lean Six Sigma Green Belt by improving a software development process, which reduced defects by 30%.</div>
 
-      <div className='about-desc-paragraph'>I have recently decided to "go back to my roots" as a software developer, and have recently completed a full-stack web development program. This program focused on the latest technologies for building reliable, client/server web-based software.</div>
+      <div className='about-desc-paragraph'>I have recently decided to "go back to my roots" as a software developer, and recently completed a full-stack web development program. This program focused on the latest technologies for building reliable, client/server web-based software. </div>
 
-      <div className='about-desc-paragraph'>I am ready to start having fun developing software, and being part of an energetic, well-focused engineering team.
+      <div className='about-desc-paragraph'>I am excited to develop web-based applications and continue learning new technologies. Most of all, I am ready to start having fun and being part of an energetic, well-focused engineering team.
       </div>
       </div>
       <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
-// import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
