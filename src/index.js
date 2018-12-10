296a480d7a7e6514691c3601427228f292dbc698
TriciaF <TriciaF@thinkful.com>
2018-12-09 20:11:22 -0500

    build

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
 import './index.css';
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
-// registerServiceWorker();
+registerServiceWorker();
